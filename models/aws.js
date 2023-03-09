f9aee5b882040ef75bd9db1c51d3be5be36f14ff
shayanthekid <sajidfayazhaniff@gmail.com>
2023-03-09 11:59:10 +0530

    removed aws keys

--- models/aws.js
+++ models/aws.js
@@ -2,9 +2,9 @@ const AWS = require('aws-sdk');
 
 // Configure the AWS SDK with your credentials
 AWS.config.update({
-    region: 'ap-southeast-1', // replace with your preferred region
-    accessKeyId: "AKIA4BBV35IBJL7GW54M",
-    secretAccessKey: "f7FK4CYj+/H9KByVxXGvdUSxSmxRuQb2IQ/IO0NB",
+    region: '', // replace with your preferred region
+    accessKeyId: "",
+    secretAccessKey: "",
     AWS_SDK_LOAD_CONFIG: "1"
 });
 
